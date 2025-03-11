93c96d0ddda29629056ed0f0ca75ca1cf0fcfd3b
AlterShot <Vigglex@mail.ru>
2025-03-11 17:15:56 +0300

    One more time

--- Ключевые слова.py
+++ Ключевые слова.py
@@ -1,14 +1,9 @@
 def max_number(a, b):
 
-    if a > b:
-        answer = a
-    elif a == b:
-        answer = a
+    if a >= b:
+        return a
     else:
-        answer = b
-
-    print(f"Max value is {answer}")
-    return answer
+        return b
 
 a = int(input("Enter first number to compare: "))
 b = int(input("Enter second number to compare: "))
@@ -21,15 +16,15 @@ def even_numbers(n):
     for i in range(n + 1):
         yield i
 
-for n in even_numbers(5):
+for n in even_numbers(3):
     if n % 2 == 0:
         print(n)
 
 def test_max_number():
-    assert max_number(10, 20) == 20, "Error, correct number is 20"
-    assert max_number(45, 5) == 45, "Error, correct number is 45"
-    assert max_number(51, 51) == 51, "Error, correct number is 51"
-    print("Tests are made")
+
+    assert max_number(10, 20) == 20, "Error, should\'ve been 20"
+    assert max_number(45, 5) == 45, "Error, should\'ve been 45"
+    assert max_number(51, 51) == 51, "Error, should\'ve been 51"
 
 test_max_number()
 
