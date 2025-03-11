1234c758917817787ff0eda6eab7604ccbd3d7f9
AlterShot <Vigglex@mail.ru>
2025-03-11 16:39:18 +0300

    Обновлено

--- Ключевые слова.py
+++ Ключевые слова.py
@@ -1,13 +1,18 @@
 def max_number(a, b):
+
     if a > b:
-        print(f"Max value is {a}")
-        return a
+        answer = a
     elif a == b:
-        print(f"These numbers are similar: {a}")
-        return a
+        answer = a
     else:
-        print(f"Max value is {b}")
-        return b
+        answer = b
+
+    print(f"Max value is {answer}")
+    return answer
+
+a = int(input("Enter first number to compare: "))
+b = int(input("Enter second number to compare: "))
+max_number(a, b)
 
 def emtpy_function():
     pass
@@ -16,17 +21,16 @@ def even_numbers(n):
     for i in range(n + 1):
         yield i
 
-for n in even_numbers(25):
+for n in even_numbers(5):
     if n % 2 == 0:
         print(n)
 
-max_number(6, 6), max_number(1, 5), max_number(24, 7)
-
 def test_max_number():
-    assert max_number(10, 20) == 20
-    assert max_number(45, 5) == 45
-    assert max_number(51, 51) == 51
+    assert max_number(10, 20) == 20, "Error, correct number is 20"
+    assert max_number(45, 5) == 45, "Error, correct number is 45"
+    assert max_number(51, 51) == 51, "Error, correct number is 51"
+    print("Tests are made")
 
 test_max_number()
-print("Tests are made")
+
 
