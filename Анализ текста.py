5305236d1d1a6c477e8f1fc8a88caa03d461a724
AlterShot <Vigglex@mail.ru>
2025-03-13 18:29:42 +0300

    Обновлены функции

--- Анализ текста.py
+++ Анализ текста.py
@@ -3,8 +3,7 @@ user_text = input("Введите ваш текст: ").lower()
 words_separate = user_text.split()
 
 def words_overall_count():
-    words_overall = len(user_text.split())
-    print(f"Всего слов в тексте: {words_overall}")
+    return len(words_separate)
 
 
 def find_longest_word():
@@ -12,7 +11,7 @@ def find_longest_word():
     for words in words_separate:
         if len(words) > len(longest_word):
             longest_word = words
-            print(f"Самое длинное слово: {longest_word}")
+    return longest_word
 
 def find_all_vowels():
     vowels_list = "аеёиоуыэюя"
@@ -21,7 +20,7 @@ def find_all_vowels():
         for vowels in words:
             if vowels in vowels_list:
                 vowel_quantity +=1
-    print(f"Гласных в тексте: {vowel_quantity}")
+    return vowel_quantity
 
 def words_found():
     for words in words_separate:
@@ -32,9 +31,13 @@ def words_found():
         print(f"Слово \"{words}\" встречается {word_quantity} раз(а)")
 
 def show_all():
-    words_overall_count()
-    find_longest_word()
-    find_all_vowels()
+    word_count = words_overall_count()
+    longest_word = find_longest_word()
+    all_vowels = find_all_vowels()
     words_found()
+
+    print(f"Всего слов в тексте: {word_count}")
+    print(f"Самое длинное слово: {longest_word}")
+    print(f"Гласных букв в тексте: {all_vowels}")
 
 show_all()
