dde8dd74084b98d31e6bf3e18b650bb0abcee956
AlterShot <Vigglex@mail.ru>
2025-03-14 12:08:47 +0300

    Обновленная версия

--- Анализ текста.py
+++ Анализ текста.py
@@ -1,43 +1,52 @@
-user_text = input("Введите ваш текст: ").lower()
+import string
 
-words_separate = user_text.split()
 
-def words_overall_count():
+def words_overall_count(words_separate):
     return len(words_separate)
 
 
-def find_longest_word():
-    longest_word = words_separate[0]
+def find_longest_word(words_separate):
+    longest_word = ""
     for words in words_separate:
         if len(words) > len(longest_word):
             longest_word = words
     return longest_word
 
+
 def find_all_vowels():
     vowels_list = "аеёиоуыэюя"
     vowel_quantity = 0
-    for words in words_separate:
-        for vowels in words:
-            if vowels in vowels_list:
-                vowel_quantity +=1
+    for vowels in user_text:
+        if vowels in vowels_list:
+            vowel_quantity += 1
     return vowel_quantity
 
-def words_found():
+
+def words_found(words_separate):
+    word_list = {}
     for words in words_separate:
-        word_quantity = 0
-        for quantity in words_separate:
-            if quantity == words:
-                word_quantity +=1
-        print(f"Слово \"{words}\" встречается {word_quantity} раз(а)")
+        if words in word_list:
+            word_list[words] += 1
+        else:
+            word_list[words] = 1
+    return word_list
+
 
 def show_all():
-    word_count = words_overall_count()
-    longest_word = find_longest_word()
+    words_separate = user_text.split()
+    word_count = words_overall_count(words_separate)
+    longest_word = find_longest_word(words_separate)
     all_vowels = find_all_vowels()
-    words_found()
-
+    word_list = words_found(words_separate)
     print(f"Всего слов в тексте: {word_count}")
     print(f"Самое длинное слово: {longest_word}")
     print(f"Гласных букв в тексте: {all_vowels}")
 
+    for word, number in word_list.items():
+        print(f"Слово \"{word}\" использовалось {number} раз(а)")
+
+
+user_entering = input("Введите ваш текст: ").lower()
+no_punctuation = str.maketrans("", "", string.punctuation)
+user_text = user_entering.translate(no_punctuation)
 show_all()
