316036e7cf699721322c2f7dea4ed3d9e42c0baa
AlterShot <Vigglex@mail.ru>
2025-03-14 19:33:30 +0300

    Обновлено

--- Замена элементов списка.py
+++ Замена элементов списка.py
@@ -1,19 +1,13 @@
-def define_list():
+def swap():
     number_list = [1, 2, 3, 4, 5]
-    return number_list
+    print(number_list)
 
-
-def swap(number_list):
     last = number_list.pop(-1)
     first = number_list.pop(0)
     number_list.insert(0, last)
     number_list.append(first)
+
     return number_list
 
 
-def printing():
-    new_list = swap(define_list())
-    print(new_list)
-
-
-printing()
+print(swap())
