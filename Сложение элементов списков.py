d17fdc053b41776abd86f5b40c2ee2fea4e18605
AlterShot <Vigglex@mail.ru>
2025-03-16 21:13:39 +0300

    Обновлено

--- Сложение элементов списков.py
+++ Сложение элементов списков.py
@@ -1,20 +1,17 @@
-def cycle():
-    list_one = [1, 2, 3, 4, 5]
-    list_two = [5, 4, 3, 2, 1, 6, 7]
+def cycle(list_one, list_two):
     new_list = []
 
     if len(list_one) > len(list_two):
-        while len(list_two) < len(list_one):
-            list_two.append(0)
-    elif len(list_two) > len(list_one):
-        while len(list_one) < len(list_two):
-            list_one.append(0)
+        max_len = len(list_one)
+    else:
+        max_len = len(list_two)
 
-    for number in range(len(list_one)):
-        result_one = list_one[number] + list_two[number]
-        new_list.append(result_one)
+    for number in range(max_len):
+        result_pair_sum = list_one[number] + list_two[number]
+        new_list.append(result_pair_sum)
+
     return new_list
 
 
-result = cycle()
+result = cycle([1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 10, 11])
 print(result)
