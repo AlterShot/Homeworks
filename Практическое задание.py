674c174d53db16b6cac287d4a4d43d3ab26c0f62
AlterShot <Vigglex@mail.ru>
2025-03-18 21:00:12 +0300

    Обновлено

--- Практическое задание.py
+++ Практическое задание.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def calculate_average(grades):
     if not grades:
         return None
@@ -16,9 +19,6 @@ def announce(students):
             print(f"Студент {name} не имеет оценок")
             continue
         average_solo = calculate_average(grade)
-        if average_solo is None:
-            print(f"Студент {name} не имеет оценок")
-            continue
         status = "Успешен" if average_solo >= 75 else "Неуспешен"
         print(f"Студент: {name}\nСредний балл: {average_solo:.2f}\nСтатус: {status}")
 
@@ -26,6 +26,8 @@ def announce(students):
 def calculate_average_overall(students):
     all_grades = []
     for student in students:
+        if not student["grades"]:
+            continue
         all_grades.extend(student["grades"])
     return sum(all_grades) / len(all_grades)
 
@@ -33,38 +35,53 @@ def calculate_average_overall(students):
 def finding_to_exclude(students):
     students_grades = [student for student in students if student["grades"]]
     student_execute = min(students_grades, key=average_grade)
-    if not student_execute["grades"]:
-        return
     minimal_grade = calculate_average(student_execute["grades"])
     print(f"Студент с наименьшим баллом: {student_execute["name"]} (Балл: {minimal_grade:.2f}) будет исключен")
     students.remove(student_execute)
+    print("Новый список: ")
+    for student in students:
+        print(f"{student["name"]}")
 
 
 def adding_student(students, name, grades):
     students.append({"name": name, "grades": grades})
 
 
+def user_interface():
+    while True:
+        begin = input(f"Выберите нужную функцию (список с баллами, общий средний, добавление студента, "
+                      f"удаление студента с низшим баллом)\nДля выхода введите \"выход\": ").lower()
+        if begin == "список с баллами":
+            announce(students)
+        elif begin == "общий средний":
+            average_all = calculate_average_overall(students)
+            print(f"Общий средний балл: {average_all:.2f}")
+        elif begin == "удаление студента с низшим баллом":
+            finding_to_exclude(students)
+        elif begin == "добавление студента":
+            name = input("Введите имя: ")
+            grade = []
+            for i in range(3):
+                while True:
+                    try:
+                        grade_input = int(input(f"Введите оценку {i + 1}: "))
+                        grade.append(grade_input)
+                        break
+                    except ValueError:
+                        print("Неверный формат. Введите еще раз: ")
+            adding_student(students, name, grade)
+            print("Новый список: ")
+            for student in students:
+                print(f"{student["name"]}")
+        elif begin == "выход":
+            sys.exit()
+
+
 students = [
     {"name": "Harry", "grades": [11, 22, 33]},
     {"name": "Bob", "grades": [12, 80, 70]},
     {"name": "Mary", "grades": [50, 60, 100]},
-    {"name": "Wilson", "grades": [88, 54, 70]},
-    {"name": "John", "grades": []},
-    {"name": "Alto", "grades": [95, 95, 92]},
-    {"name": "Tom", "grades": [61, 44, 98]},
-    {"name": "Roxie", "grades": [83, 99, 100]}
+    {"name": "Wilson", "grades": [88, 54, 70]}
 ]
 
-adding_student(students, "Boris", [100, 90, 100])
-
-announce(students)
-
-average_all = calculate_average_overall(students)
-
-print(f"Общий средний балл: {average_all:.2f}")
-
-finding_to_exclude(students)
-
-print("Новый список: ")
-for student in students:
-    print(f"{student["name"]}")
+user_interface()
