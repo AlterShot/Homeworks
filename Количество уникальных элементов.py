543ff56defe9e66621dddb4d0b83f60ecbc2226d
AlterShot <Vigglex@mail.ru>
2025-03-19 14:10:42 +0300

    Обновлено

--- Количество уникальных элементов.py
+++ Количество уникальных элементов.py
@@ -1,3 +1,18 @@
-this_list = [1, 2, 2, 4, 5, 6, 6, 7, 6, 4, 2, 4, 6]
-unique_list = set(this_list)
-print(f"Уникальных значений: {len(unique_list)}")
+def counting_unique(list):
+    while True:
+        user_entering = input(f"Введите число (для завершения введите \"q\"): ")
+        if user_entering == "q":
+            break
+        try:
+            user_entering = int(user_entering)
+            this_list.append(user_entering)
+        except ValueError:
+            print("Нужно ввести число")
+    return this_list
+
+
+this_list = []
+
+u_list = set(counting_unique(this_list))
+
+print(f"Уникальных значений: {len(u_list)}")
