8a47a1f829f0eb292fe43da8d062607d997b8c31
AlterShot <Vigglex@mail.ru>
2025-03-26 19:53:39 +0300

    обновлено

--- Class/todolist.py
+++ Class/todolist.py
@@ -1,36 +1,41 @@
 class ToDoList:
     def __init__(self):
-        self.tasks = []
+        self.tasks = {}
 
     def add_task(self, task):
-        self.tasks.append({"do": task, "status": False})
+        check_task = task.strip().lower()
+
+        if check_task not in self.tasks:
+            self.tasks[check_task] = {"status": False}
+            print(f"Задача \"{task}\" добавлена")
+        else:
+            print(f"Задача \"{task}\" уже существует")
 
     def complete_task(self, task):
-        task_find = False
-        for do in self.tasks:
-            if do["do"] == task and not do["status"]:
-                do["status"] = True
-                print(f"Задача \"{task}\" отмечена как выполненная")
-                task_find = True
-                break
-        if not task_find:
-            print("Задача не обнаружена")
+        check_task = task.strip().lower()
+
+        if check_task in self.tasks:
+            if not self.tasks[check_task]["status"]:
+                self.tasks[check_task]["status"] = True
+                print(f"Задача \"{task}\" отмечена выполненной")
+            else:
+                print(f"Задача \"{task}\" уже выполненна")
+        else:
+            print(f"Задача не найдена")
 
     def remove_task(self, task):
-        task_find = False
-        for do in self.tasks:
-            if do["do"] == task:
-                self.tasks.remove(do)
-                print(f"Задача \"{task}\" удалена")
-                task_find = True
-                break
-        if not task_find:
-            print("Задача не обнаружена")
+        check_task = task.strip().lower()
+
+        if check_task in self.tasks:
+            del self.tasks[check_task]
+            print(f"Задача \"{task}\" удалена")
+        else:
+            print(f"Задача не найдена")
 
     def list_tasks(self):
         if not self.tasks:
             print("Задач пока нет.")
         else:
-            for do in self.tasks:
-                status = "\u2705" if do["status"] else "\u274C"
-                print(f"\n{status}: {do["do"]}")
+            for task, status in self.tasks.items():
+                t_status = "\u2705" if status["status"] else "\u274C"
+                print(f"\n{t_status}: {task.capitalize()}")
